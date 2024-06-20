285c5416469c5c9a1281c34c290fa8d20769295f
Akshay Agrawal <akshay2agrawal@gmail.com>
2024-06-20 18:49:56 +0200

    fead: custom middleware

--- Express Server/index.js
+++ Express Server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
-
+import morgan from "morgan";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
@@ -13,6 +13,9 @@ var jsonParser = bodyParser.json();
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
+app.use(morgan("combined"));
+app.use(customMiddleware);
+
 // POST /login gets urlencoded bodies
 app.post("/submit", urlencodedParser, function (req, res) {
   console.log(req.body);
@@ -21,6 +24,8 @@ app.post("/submit", urlencodedParser, function (req, res) {
 // POST /api/users gets JSON bodies
 app.post("/api/users", jsonParser, function (req, res) {
   // create user in req.body
+  console.log("user route");
+  res.send("user route" + req.body);
 });
 
 app.get("/", (req, res) => {
@@ -30,3 +35,8 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on Port ${port}`);
 });
+
+function customMiddleware(req, res, next) {
+  console.log("Custom Middleware executed");
+  next();
+}
